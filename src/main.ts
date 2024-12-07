2397a511c79f99a1e9f50e8589bf284c09cb5ac0
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2024-12-07 12:53:22 +0530

    Refactor main.ts to remove unused code and simplify server initialization

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { Callback, Context, Handler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
-
-let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,16 +16,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.init();
-  
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  return app;
 }
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+if (require.main === module) {
+  bootstrap().then(app => {
+    app.listen(3000);
+    console.log('Server running on port 3000');
+  });
+}
+
+export default bootstrap;
